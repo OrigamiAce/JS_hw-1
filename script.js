e6af5769cc00d256e6cab5aa69cc2241893e7b53
OrigamiAce <115983484+OrigamiAce@users.noreply.github.com>
2023-07-10 12:53:38 +0300

    js

---
+++ script.js
@@ -0,0 +1,35 @@
+// let a = 4
+// let b = 55
+//
+// console.log(a * b)
+// console.log(a + b)
+// console.log(a / b)
+// console.log(a % b)
+
+// let a = 'Hello,'
+// let b = 'Ruslan'
+//
+// console.log(a + ' ' + b)
+
+
+// let a = 7
+// let b = 'wdw'
+//
+// console.log(a * b)
+
+
+// let a = 5
+// let b ='5'
+//
+// console.log(a + +b)
+// console.log(a === b)
+
+
+
+// let c = 2
+// let b = undefined
+//
+// console.log(c + b)
+// console.log(c > b)
+// console.log(b + c)
+// console.log(undefined + 2)
